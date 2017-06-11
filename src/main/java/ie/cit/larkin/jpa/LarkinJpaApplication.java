32528da4296cd79c2f254c6994a92cfb9ce87da8
Larkin Cunningham <larkin.cunningham@cit.ie>
2017-06-12 00:18:05 +0100

    Added project

---
+++ src/main/java/ie/cit/larkin/jpa/LarkinJpaApplication.java
@@ -0,0 +1,12 @@
+package ie.cit.larkin.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarkinJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LarkinJpaApplication.class, args);
+	}
+}
---
+++ src/main/java/ie/cit/larkin/jpa/controller/HomeController.java
@@ -0,0 +1,15 @@
+package ie.cit.larkin.jpa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+}
